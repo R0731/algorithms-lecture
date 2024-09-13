ac185061cad3855588cb2741e6392c9411adafb2
JIEUN <jsilver0127@gmail.com>
2024-09-13 15:59:02 +0900

    Sep 13

---
+++ src/aps/알고리즘응용/패턴매칭_01_KMP.java
@@ -0,0 +1,51 @@
+package aps.알고리즘응용;
+
+public class 패턴매칭_01_KMP {
+    public static void main(String[] args) {
+        String text = "ABABABACABAABABACACA";
+        String pattern = "ABABACA";
+
+        KMP(text, pattern);
+    }//main
+    //T : Text
+    //P : Pattern
+    public static void KMP(String T, String P){
+        int[] pi = getPi(P); //점프테이블(실패함수테이블) 준비시킨다
+
+        int j = 0; //패턴의 인덱스
+        //i : 본문의 인덱스
+        for(int i = 0; i < T.length(); i++){
+            //달랐을 때
+            while(j > 0 && T.charAt(i) != P.charAt(j)){
+                j = pi[j - 1]; //점프
+            }
+            //같았을 때
+            if(T.charAt(i) == P.charAt(j)) {
+                if (j == P.length() - 1) {
+                    System.out.println((i - P.length() + 1) + "위치부터 시작");
+                    j = pi[j];
+                } else {
+                    j++;
+                }
+            }
+        }
+    }
+    //pi[] : 패턴 p를 i인덱스까지 잘라서(부분문자열)
+    //접두사와 접미사가 일치하는 최대 길이를 담을 배열
+    public static int[] getPi(String P){
+        int[] pi = new int[P.length()];
+
+        int j = 0; //여기까지는 같음
+        for(int i = 1; i < P.length(); i++){
+            //i와 j가 가리키는 값이 다르면
+            while(j > 0 && P.charAt(i) != P.charAt(j))
+                j = pi[j - 1];
+
+            //i와 j가 가리키는 값이 같다면
+            if(P.charAt(i) == P.charAt(j)){
+                pi[i] = ++j;
+            }
+        }
+        return pi;
+    }
+}
